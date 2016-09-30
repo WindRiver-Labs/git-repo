06406c57455d7218b486408b7659e2a009a73ef0
Mark Hatle <mark.hatle@windriver.com>
2016-09-30 11:39:48 -0500

    subcmd/status.py: Limit status to non-bare projects
    Skip bare projects.  These can not be checked out, so any status will be
    incorrect.
    
    Signed-off-by: Mark Hatle <mark.hatle@windriver.com>

--- subcmds/status.py
+++ subcmds/status.py
@@ -137,6 +137,8 @@ the following meanings:
 
     if opt.jobs == 1:
       for project in all_projects:
+        if project.bare == 'True':
+          continue
         state = project.PrintWorkTreeStatus(quiet=opt.quiet)
         if state == 'CLEAN':
           next(counter)
@@ -144,6 +146,8 @@ the following meanings:
       sem = _threading.Semaphore(opt.jobs)
       threads = []
       for project in all_projects:
+        if project.bare == 'True':
+          continue
         sem.acquire()
 
         t = _threading.Thread(target=self._StatusHelper,
@@ -160,6 +164,8 @@ the following meanings:
       proj_dirs = set()
       proj_dirs_parents = set()
       for project in self.GetProjects(None, missing_ok=True):
+        if project.bare == 'True':
+          continue
         proj_dirs.add(project.relpath)
         (head, _tail) = os.path.split(project.relpath)
         while head != "":
